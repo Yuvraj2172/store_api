bbc930953eaff35b674b5717c4e2087d6d6c6f24
Yuvraj Soni <demonsroar2172@gmail.com>
2024-02-11 00:40:01 +0530

    added regex for name

--- controllers/product.js
+++ controllers/product.js
@@ -1,18 +1,31 @@
 const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ price: { $gte: 120 } });
+  const search = "ab";
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  });
   res.status(200).json({ products, nHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   // console.log(req.query);
-  const { featured } = req.query;
+  const { featured, company, name, rating } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-  console.log(queryObject);
-  console.log(req.query);
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (rating) {
+    queryObject.rating = Number(rating);
+    // console.log(typeof rating);
+  }
+  // console.log(queryObject);
+  // console.log(req.query);
   // const products = await Product.find(req.query);
   const products = await Product.find(queryObject);
   res.status(200).json({ nHits: products.length, products });
