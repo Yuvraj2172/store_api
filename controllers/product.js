f4cc4fb101aca46914f828b660cb1acd387b82d8
Yuvraj Soni <demonsroar2172@gmail.com>
2024-02-10 21:46:49 +0530

    refactor to query object

--- controllers/product.js
+++ controllers/product.js
@@ -1,11 +1,21 @@
 const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ price : {$gte : 120} });
+  const products = await Product.find({ price: { $gte: 120 } });
   res.status(200).json({ products, nHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products  route " });
+  // console.log(req.query);
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryObject);
+  console.log(req.query);
+  // const products = await Product.find(req.query);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ nHits: products.length, products });
 };
 
 module.exports = {
